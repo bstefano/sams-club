bbf1a83aafecfcc7a6debedd356252392fd09d6a
Bruno Daniel de Stefano <brunodanieldestefano@mac.lan>
2025-06-15 21:13:52 -0300

    first commit

---
+++ pages/[slug].js
@@ -0,0 +1,22 @@
+import { getLandingPages } from '../lib/datocms'
+
+export async function getStaticPaths() {
+  const data = await getLandingPages()
+  const paths = data.allLandingPages.map(p => ({ params: { slug: p.slug } }))
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+  const data = await getLandingPages()
+  const page = data.allLandingPages.find(p => p.slug === params.slug)
+  return { props: { page } }
+}
+
+export default function LandingPage({ page }) {
+  return (
+    <div>
+      <h1>{page.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: page.content }} />
+    </div>
+  )
+}
